6adce7a4fc9c97ed615439f6b5ee78a7a9f4805e
javaherobrine <49926366+javaherobrine@users.noreply.github.com>
2022-12-04 11:48:57 +0800

    Make the craftgame-tcp simpler but it still can be used

--- io/github/javaherobrine/net/ShakeHandsMessage.java
+++
@@ -1,9 +0,0 @@
-package io.github.javaherobrine.net;
-import java.beans.*;
-@JavaBean
-public class ShakeHandsMessage {
-	public int id;
-	public TransmissionStatus status;
-	public TransmissionFormat format;
-	public boolean connected;
-}
