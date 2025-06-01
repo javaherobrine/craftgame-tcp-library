62d0e364ab2c939376304d7d353735abb65cb8ce
javaherobrine <49926366+javaherobrine@users.noreply.github.com>
2025-06-02 00:59:57 +0800

    Make it synchronize with LovelyZeeiam's version(LovelyZeeiam/JaroSideCraftGame)

---
+++ io/github/javaherobrine/Constants.java
@@ -0,0 +1,8 @@
+package io.github.javaherobrine;
+import java.util.*;
+import io.github.javaherobrine.net.*;
+public final class Constants {
+	private Constants() {}
+	public static final int versionID=1851844483;
+	public static final ArrayList<NetworkEventListener> REGISTERED_NETWORK_EVENT_LISTENER=new ArrayList<>();
+}
